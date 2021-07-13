645892bef082d84e92896b61462bbe3d5ec35037
sasaki957 <79430292+sasaki957@users.noreply.github.com>
2021-07-13 10:25:56 +0900

    Update test_line.php

--- test_line.php
+++ test_line.php
@@ -1,2 +1,18 @@
-<?php
-echo 'test';
+// LINEからきたPOST値（JSON）を取得
+$json_string = file_get_contents('php://input');
+$json_obj = json_decode($json_string);
+
+// タイプとユーザID
+$type = $json_obj->events[0]->type . "\n";
+$user_id = $json_obj->events[0]->source->userId . "\n";
+
+// ファイル出力
+file_put_contents(
+    'info.php',
+    $type . $user_id
+);
+
+file_put_contents(
+    'log.php',
+    print_r($json_obj, true)
+);
